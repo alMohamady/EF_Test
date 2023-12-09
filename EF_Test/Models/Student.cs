59d217d227f266886bfbc06ee48631a2182149b4
al Mohamady <al.Mohamady47@gmail.com>
2023-12-09 21:30:35 +0200

    04 Create Domain Model Classes

---
+++ EF_Test/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Test.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public int Grade { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
