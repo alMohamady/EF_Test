29333d78aaedc1250083c147d16ce1be1d7b31cb
al Mohamady <al.Mohamady47@gmail.com>
2023-12-09 22:28:15 +0200

    05 Create DbContext

---
+++ EF_Test/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EF_Test.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Test
+{
+    public class AppDbContext : DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Connections.sqlConStr);
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
