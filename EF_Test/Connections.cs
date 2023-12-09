3a4740a5bb48bc10cd470e5b7aec175e1fa1081a
al Mohamady <al.Mohamady47@gmail.com>
2023-12-09 20:50:46 +0200

    03 Create SQL Connection string

---
+++ EF_Test/Connections.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Test
+{
+    internal static class Connections
+    {
+        public const string sqlConStr = """
+               Server=DESKTOP-E0QD2TF\SQL2019;
+               Database=dbEfTest;
+               User Id=sa;
+               Password=123456;
+               TrustServerCertificate=True;
+            """;
+    }
+}
