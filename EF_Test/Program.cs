6d4238c80d55e17a6915ac188de94e6e5a9f168b
al Mohamady <al.Mohamady47@gmail.com>
2023-12-19 15:22:37 +0200

    12 CRUD Operations in Entity Framework Core

--- EF_Test/Program.cs
+++ EF_Test/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EF_Test;
+using EF_Test.Models;
+
 Console.WriteLine("Hello, World!");
+
+
+using var db = new AppDbContext();
+
+// Insert
+//var department = new Department()
+//{
+//    Name = "Ahmed"
+//};
+//db.Departments.Add(department);
+
+var department = db.Departments.Find(1);
+
+if (department != null)
+{
+    Console.WriteLine(department.Name);
+
+    //Edit
+    //department.Name = "Class 1";
+    //db.SaveChanges();
+
+    //Delete 
+    db.Departments.Remove(department);
+    db.SaveChanges();
+}
+else
+{
+    Console.WriteLine("nothing");
+}
