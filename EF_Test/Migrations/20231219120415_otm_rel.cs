987ae03720fd3085ee628ecfff10cc9ce6d49ba3
al Mohamady <al.Mohamady47@gmail.com>
2023-12-19 14:09:35 +0200

    09 One to many relationship in EF

---
+++ EF_Test/Migrations/20231219120415_otm_rel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Test.Migrations
+{
+    /// <inheritdoc />
+    public partial class otm_rel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "departmentId",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_departmentId",
+                table: "Students",
+                column: "departmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Departments_departmentId",
+                table: "Students",
+                column: "departmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Departments_departmentId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_departmentId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "departmentId",
+                table: "Students");
+        }
+    }
+}
--- EF_Test/Models/Student.cs
+++ EF_Test/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +18,9 @@ namespace EF_Test.Models
         public int Grade { get; set; }
         public DateTime Birthdate { get; set; }
         public Grade grade { get; set; }
+
+        [ForeignKey("department")]
+        public int departmentId { get; set; }
+        public Department department { get; set; }
     }
 }
