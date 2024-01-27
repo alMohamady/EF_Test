a1e7f0abb949ecfaa60cc1ff71ad02f5a3d3d695
al Mohamady <al.Mohamady47@gmail.com>
2024-01-27 20:42:24 +0200

    36 Using Transactions - EF Core

--- EF_Test/Program.cs
+++ EF_Test/Program.cs
@@ -9,12 +9,42 @@ Console.WriteLine("Hello, World!");
 
 var db = new AppDbContext();
 
-var stu = db.Students.SingleOrDefault(s => s.Id == 3);
+using var transaction = db.Database.BeginTransaction();
 
-Console.WriteLine(stu.department.Name);
-
-foreach (var book in stu.books)
+try
 {
-    Console.WriteLine(book.book.Name);
+    db.Departments.Add(new Department { Name = "tarna 11", des = "edc" });
+    db.SaveChanges();
+
+    db.Departments.Add(new Department { Name = "tarna 44", des = "edc4" });
+    db.SaveChanges();
+
+    transaction.CreateSavepoint("data_ok");
+
+    db.Departments.Add(new Department { Name = "tarna 33", des = "edc1" });
+    db.SaveChanges();
+
+    db.Departments.Add(new Department { Name = "tarna 22", des = "edcjjghghghg" });
+    db.SaveChanges();
+
+
+    transaction.Commit();
 }
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    //transaction.Rollback();
+    try
+    {
+        transaction.RollbackToSavepoint("data_ok");
+        transaction.Commit();
+    }
+    catch (Exception l2ex)
+    {
+        Console.WriteLine(l2ex.Message);
+        transaction.Rollback();
+    }
+}
+
+
 
