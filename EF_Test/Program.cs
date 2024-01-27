d05c17e8c0c60d894ccbf728540f33877bf33db9
al Mohamady <al.Mohamady47@gmail.com>
2024-01-27 17:26:36 +0200

    34 Explicit Loading of Related Data in EF Core

--- EF_Test/Program.cs
+++ EF_Test/Program.cs
@@ -9,16 +9,21 @@ Console.WriteLine("Hello, World!");
 
 var db = new AppDbContext();
 
-var stu = db.Students.Include(d => d.department)
-                     .Include(g => g.grade)
-                     .Include(b => b.books)
-                     .ThenInclude(sb => sb.book)
-                     .SingleOrDefault(s => s.Id == 3);
+var stu = db.Students.SingleOrDefault(s => s.Id == 3);
 
+
+
+db.Entry(stu).Reference(d => d.department).Load();
 Console.WriteLine(stu.department.Name);
+
+
+//db.Entry(stu).Collection(st => st.books).Load();
+
+db.Entry(stu).Collection(st => st.books).Query().Where(x => x.bookId == 3).ToList();
 
 foreach (var book in stu.books)
 {
+    db.Entry(book).Reference(b => b.book).Load();
     Console.WriteLine(book.book.Name);
 }
 
